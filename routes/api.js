e6388dfccb7a626a9c3a6073738693f4d6490923
RMS <rene.as.himself@gmail.com>
2019-02-04 23:48:44 -0600

    Added helmet configuration

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,35 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+'use strict'
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
+const expect = require('chai').expect
+const MongoClient = require('mongodb')
+const ObjectId = require('mongodb').ObjectID
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = function(app) {
+    app.route('/api/issues/:project')
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-      
-    });
-    
-};
+        .get(function(req, res) {
+            const project = req.params.project
+        })
+
+        .post(function(req, res) {
+            const project = req.params.project
+        })
+
+        .put(function(req, res) {
+            const project = req.params.project
+        })
+
+        .delete(function(req, res) {
+            const project = req.params.project
+        })
+}
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,15 +1,13 @@
 /*
-*
-*
-*       FILL IN EACH UNIT TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH UNIT TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chai = require('chai');
+const chai = require('chai')
 
-suite('Unit Tests', function(){
-
-//none requiered
-
-});
+suite('Unit Tests', function() {
+    //none requiered
+})
